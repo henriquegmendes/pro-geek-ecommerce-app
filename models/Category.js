3366c508fd51f8dfa61684c8c0c82b50bae3b1a1
brudolce <brudolce@gmail.com>
2019-02-23 20:29:21 -0300

    created models

---
+++ models/Category.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const mySchema = new Schema({
+    name: String,
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    }], 
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+const Category = mongoose.model('Category', mySchema);
+
+module.exports = Category;
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const mySchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    birthDate: { type: String, required: true },
+    password: { type: String, required: true },
+    address: {
+        street: String,
+        complement: String,
+        postalCode: String
+    },
+    userAvaliations: Array,
+    userOrders: Array,
+    role: {
+        type: String,
+        enum: ['User', 'Admin'],
+        default: 'User'
+    },
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+const User = mongoose.model('User', mySchema);
+
+module.exports = User;
